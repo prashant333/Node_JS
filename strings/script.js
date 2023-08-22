9bd8ae5e00908700ae699f26a4f5d4381b1fcdb1
prashant <pmani157@gmail.com>
2023-08-22 22:28:48 +0530

    string manupulations

--- strings/script.js
+++ strings/script.js
@@ -11,13 +11,22 @@ x= `Hello my name is ${name} and I am ${age} years old.`;
 
 // String properties and methods
 
-const s = 'Hello World'
+const s = new String('Hello World')
 
 x = s.length
 // When we run any property or method on primitive data type, JS using
 // its wrapper cretes an object and then property or method is applied. 
 // const s = new String("Hello World");
 
+// objects have prototype, and that is where all the method is stored.
 
+x = s.__proto__;
 
+x = s.replace("World", "Prashant")
+
+x=  s.includes("world")
+
+x= s.split()
+x= s.split(" ")
+x = s.split('')
 console.log(x)
